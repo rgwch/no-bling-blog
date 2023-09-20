b6d78ff15ed8d10236888a2117b302bdabcf6bd6
rgwch <weirich@elexis.ch>
2023-09-20 16:05:16 +0200

    initial import

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+type post = {
+    id: string
+    heading: string
+    shorttext: string
+    fulltext: string
+    keywords: string
+    category: string
+    author: string
+    created: Date
+    modified: Date
+    published: boolean
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import '../app.css';
+</script>
+
+<h1>The No-Bling Blog</h1>
