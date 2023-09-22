9631cefc1b5c74e8272b57f7ceb400a21a520933
gerry <weirich@elexis.ch>
2023-09-22 17:10:08 +0200

    details

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,30 @@
 <script>
-    import '../app.css';
+    import "../app.css";
 </script>
 
 <h1>The No-Bling Blog</h1>
 
 <style>
-        .response {
-    margin-top: 100px;
-    width: 100%;
-    display: flex;
-	flex-direction: row;
-    flex-wrap: wrap;
+    .response {
+        margin-top: 100px;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
     }
-.data {
-    font-family: 'Lato', sans-serif;
-    margin: 20px;
-    padding: 20px;
-    border-radius: 15px;
-	background-color: #242D3D;
-	color: #1EBAD5;
-	display: flex;
-	flex-direction: row;
-    flex-wrap: wrap;
-	justify-content: space-around;
-	align-items: center;
-	text-align: center;
-    width: 25%;
-}
-
+    .data {
+        font-family: "Lato", sans-serif;
+        margin: 20px;
+        padding: 20px;
+        border-radius: 15px;
+        background-color: #242d3d;
+        color: #1ebad5;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+        width: 25%;
+    }
 </style>
