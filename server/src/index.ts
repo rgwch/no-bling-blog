7c123c31233a40a64e62d790aca22181c331ac1a
rgwch <weirich@elexis.ch>
2023-09-26 08:08:12 +0200

    hono server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
-import { Hono } from 'hono'
+import { Hono} from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
 
 const app = new Hono()
 
+app.use("/static/",serveStatic({path: "./"}))
 app.get('/', (c) => c.text('Hello Hono!'))
 
 export default app
