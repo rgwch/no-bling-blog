d5ae0298a3d4629e5540b206bcfbf210f8815da2
gerry <weirich@elexis.ch>
2023-12-05 18:29:30 +0100

    aktivate users

--- client/src/lib/views/Users.svelte
+++ client/src/lib/views/Users.svelte
@@ -3,6 +3,11 @@
     import { request } from "../io";
     import { _ } from "svelte-i18n";
     let users: Array<user> = [];
+    let newUser: user = {
+        name: "",
+        label: "",
+        role: "editor",
+    };
     request("admin/users").then((result) => {
         users = result.map((u: user) => {
             u.label = u.label ?? u.name;
@@ -11,9 +16,21 @@
     });
     async function remove(user: user) {
         await request(`admin/deluser/${user.name}`);
+        users = users.filter((u) => u.name !== user.name);
     }
     async function modify(user: user) {
         await request(`admin/moduser/${user.name}`);
+    }
+    async function addUser() {
+        await request(
+            `admin/adduser/${newUser.name}/${newUser.role}/${newUser.label}`,
+        );
+        users=[...users,newUser]
+        newUser = {
+            name: "",
+            label: "",
+            role: "editor",
+        };
     }
 </script>
 
@@ -21,10 +38,10 @@
     <table>
         <thead>
             <tr class="text-left">
-                <th>{$_('username')}</th>
-                <th>{$_('label')}</th>
-                <th>{$_('role')}</th>
-                <th>{$_('actions')}</th>
+                <th>{$_("username")}</th>
+                <th>{$_("label")}</th>
+                <th>{$_("role")}</th>
+                <th>{$_("actions")}</th>
             </tr>
         </thead>
         <tbody>
@@ -39,16 +56,33 @@
                         </select>
                     </td>
                     <td class="p-4">
-                        <button class="btn">{$_('delete')}</button>
-                        <button class="btn">{$_('edit')}</button>
+                        <button class="btn" on:click={() => remove(user)}
+                            >{$_("delete")}</button>
+                        <button class="btn" on:click={() => modify(user)}
+                            >{$_("edit")}</button>
                     </td>
                 </tr>
             {/each}
             <tr>
-                <td><input type="text" placeholder={$_('username')} /></td>
-                <td><input type="text" placeholder={$_('label')} /></td>
-                <td><input type="text" placeholder={$_('role')} /></td>
-                <td><button class="btn mx-3">{$_('add')}</button></td>
+                <td
+                    ><input
+                        type="text"
+                        placeholder={$_("username")}
+                        bind:value={newUser.name} /></td>
+                <td
+                    ><input
+                        type="text"
+                        placeholder={$_("label")}
+                        bind:value={newUser.label} /></td>
+                <td
+                    ><input
+                        type="text"
+                        placeholder={$_("role")}
+                        bind:value={newUser.role} /></td>
+                <td
+                    ><button class="btn mx-3" on:click={addUser}
+                        >{$_("add")}</button
+                    ></td>
             </tr>
         </tbody>
     </table>
